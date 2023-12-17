7fbaecfdc35fb68bdcce1c53b99c48493e094e55
Dan <danwill1210@gmail.com>
2023-12-17 20:43:49 +0000

    Initial commit

---
+++ src/main/java/com/example/automanga/AutoMangaApplication.java
@@ -0,0 +1,13 @@
+package com.example.automanga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMangaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoMangaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/automanga/AutoMangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automanga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMangaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
